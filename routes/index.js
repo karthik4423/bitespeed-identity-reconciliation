c245ef9b42eb9b395a42b265f8f310980b8f0d01
Karthik Raj <karthikraj4423@gmail.com>
2023-06-16 22:57:07 +0530

    adde logic to handle secondary contact addition and updation

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const serializer = require('../serializers/index');
 const controller = require('../controllers/index');
 
 router.post('/identify', async (req, res) => {
   try {
-    const response = serializer.identityReconciliation(await controller.handleIdentify(req));
-    return res.status(200).send({ msg: 'Success', response });
+    const response = await controller.handleIdentify(req);
+    return res.status(response?.status || 200).send({ msg: 'Success', response });
   } catch (err) {
+    console.log(err);
     return res.status(err.status).send({ msg: err.message, response: err });
   }
 });
--- services/index.js
+++ services/index.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
 const managers = require('../managers/index');
+const serializers = require('../serializers');
 
 const validateInput = ({ email, phone }) => {
   if (email === null && phone === null) {
@@ -24,6 +25,17 @@ const validateInput = ({ email, phone }) => {
     };
   }
 };
+const createContactModel = ({ currentTimeStamp, linkPrecedence, linkedId, email, phone }) => {
+  return {
+    phoneNumber: phone,
+    email,
+    linkedId,
+    linkPrecedence,
+    createdAt: currentTimeStamp,
+    updatedAt: currentTimeStamp,
+    deletedAt: 'NULL'
+  };
+};
 
 const serviceFunctions = {
   reconcileAccounts: async ({ email = null, phone = null }) => {
@@ -31,29 +43,110 @@ const serviceFunctions = {
       // Validate the input
       validateInput({ phone, email });
 
-      const contactDetails = await managers.getContacts({ phone, email });
+      const contactDetails = await managers.getContactsByPhoneAndEmail({ phone, email });
 
       // If no contact details are fetched, add a new contact entry to the table
-      const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
-
       if (!contactDetails.length) {
-        const model = {
-          phoneNumber: phone,
-          email,
-          linkedId: null,
+        const model = createContactModel({
+          currentTimeStamp: moment().format('YYYY-MM-DD HH:mm:ss'),
           linkPrecedence: 'primary',
-          createdAt: currentTime,
-          updatedAt: currentTime,
-          deletedAt: null
-        };
-        await managers.createContacts({ model });
-        return {
-          status: 201,
-          message: 'New contact created'
-        };
+          linkedId: null,
+          email,
+          phone
+        });
+        const contactId = await managers.createContacts({ model });
+        return serializers.identityReconciliation({
+          contacts: [model],
+          primaryContactId: contactId
+        });
       }
+
+      // Format the query result to decide how to reconcile the contacts
+      let isANewEmail = true;
+      let isANewPhone = true;
+      const contactIdsToMarkAsSecondary = [];
+      contactDetails.forEach((contact, index) => {
+        if (contact.email === email) {
+          isANewEmail = false;
+        }
+        if (parseInt(contact.phoneNumber, 10) === phone) {
+          isANewPhone = false;
+        }
+        if (
+          index &&
+          (contact.linkPrecedence === 'primary' ||
+            contact.linkedId !== (contactDetails[0].linkedId || contactDetails[0].id))
+        ) {
+          contactIdsToMarkAsSecondary.push(contact.id);
+        }
+      });
+      // If the request has a new phone or email, create a new secondary contact
+      let createSecondaryContactResult = 0;
+      let secondaryContactModel = {};
+      if (isANewEmail || isANewPhone) {
+        secondaryContactModel = createContactModel({
+          currentTimeStamp: moment().format('YYYY-MM-DD HH:mm:ss'),
+          linkPrecedence: 'secondary',
+          linkedId: contactDetails[0].linkedId || contactDetails[0].id,
+          email,
+          phone
+        });
+        createSecondaryContactResult = await managers.createContacts({
+          model: secondaryContactModel
+        });
+      }
+
+      // Update any primary contact that can be linked to this
+      let linkedContactsOfSecondaryContacts = [];
+      if (contactIdsToMarkAsSecondary.length) {
+        linkedContactsOfSecondaryContacts = await managers.getContactsByLinkedId({
+          linkedId: contactIdsToMarkAsSecondary
+        });
+        const contactIdsToUpdate = [
+          ...linkedContactsOfSecondaryContacts.map((contact) => contact.id),
+          ...contactIdsToMarkAsSecondary
+        ];
+        await managers.updateContacts({
+          ids: contactIdsToUpdate,
+          model: {
+            linkPrecedence: 'secondary',
+            linkedId: contactDetails[0].linkedId || contactDetails[0].id
+          }
+        });
+      }
+
+      // Fetch all the secondary contacts with the same linkedId that doesn't have the matching email or phone
+      const allLinkedContactsOfPrimaryContact = await managers.getContactsByLinkedId({
+        linkedId: contactDetails[0].linkedId || contactDetails[0].id
+      });
+      if (createSecondaryContactResult) {
+        linkedContactsOfSecondaryContacts.push({
+          ...secondaryContactModel,
+          id: createSecondaryContactResult
+        });
+      }
+      const availableContacts = [
+        ...allLinkedContactsOfPrimaryContact,
+        ...contactDetails,
+        ...(linkedContactsOfSecondaryContacts.length ? linkedContactsOfSecondaryContacts : [])
+      ];
+
+      // Will fetch the primary contact's details if it isn't already available with us
+      const isPrimaryContactAvailable = availableContacts.map(
+        (contact) => contact.id === contactDetails[0].linkedId
+      ).length;
+      return serializers.identityReconciliation({
+        contacts: [
+          ...availableContacts,
+          ...(isPrimaryContactAvailable
+            ? await managers.getContactsById({
+                id: contactDetails[0].linkedId || contactDetails[0].id
+              })
+            : [])
+        ],
+        primaryContactId: contactDetails[0].linkedId || contactDetails[0].id
+      });
     } catch (err) {
-      console.log(err);
       throw err;
     }
   }
