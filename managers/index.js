953c31db9063546ff389a42542beb03dc050a847
Karthik Raj <karthikraj4423@gmail.com>
2023-06-17 01:59:48 +0530

    updated syntax for queries

--- managers/index.js
+++ managers/index.js
@@ -15,7 +15,7 @@ const managerFunctions = {
     if (phone || email) {
       queryString += ` WHERE ${phone ? `phoneNumber = ${phone}` : ``} ${
         phone && email ? ` OR ` : ``
-      } ${email ? `email = "${email}"` : ``}`;
+      } ${email ? `email = '${email}'` : ``}`;
     }
     queryString += ` ORDER BY createdAt;`;
     const result = await dbQuery(queryString);
@@ -34,15 +34,15 @@ const managerFunctions = {
   createContacts: async ({ model }) => {
     const queryString = `INSERT INTO CONTACT (${contactTableKeyNames.join(',')}) VALUES (${
       model.phoneNumber || 'NULL'
-    }, ${model.email ? `"${model.email}"` : 'NULL'}, ${model.linkedId}, "${
+    }, ${model.email ? `'${model.email}'` : 'NULL'}, ${model.linkedId}, '${
       model.linkPrecedence
-    }", "${model.createdAt}", "${model.updatedAt}", ${model.deletedAt});`;
+    }', '${model.createdAt}', '${model.updatedAt}', ${model.deletedAt});`;
     const result = await dbQuery(queryString);
     return result.insertId;
   },
   updateContacts: async ({ ids, model }) => {
     const queryString = `UPDATE CONTACT SET ${Object.keys(model).map(
-      (key) => `${key} = ${typeof model[key] === 'string' ? `"${model[key]}"` : `${model[key]}`}`
+      (key) => `${key} = ${typeof model[key] === 'string' ? `'${model[key]}'` : `${model[key]}`}`
     )} WHERE id IN (${ids});`;
     await dbQuery(queryString);
   }
