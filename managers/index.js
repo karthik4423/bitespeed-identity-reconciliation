1b2e83e770ed7e336c05cafbbdd0ad3a2c5646e5
Karthik Raj <karthikraj4423@gmail.com>
2023-06-16 22:56:21 +0530

    added manager fuctions

--- managers/index.js
+++ managers/index.js
@@ -10,23 +10,40 @@ const contactTableKeyNames = [
   'deletedAt'
 ];
 const managerFunctions = {
-  getContacts: async ({ phone, email }) => {
+  getContactsByPhoneAndEmail: async ({ phone, email }) => {
     let queryString = `SELECT * FROM CONTACT`;
     if (phone || email) {
       queryString += ` WHERE ${phone ? `phoneNumber = ${phone}` : ``} ${
         phone && email ? ` OR ` : ``
-      } ${email ? `email = ${email}` : ``}`;
+      } ${email ? `email = "${email}"` : ``}`;
     }
     queryString += ` ORDER BY createdAt;`;
     const result = await dbQuery(queryString);
     return result;
   },
+  getContactsById: async ({ id }) => {
+    let queryString = `SELECT * FROM CONTACT WHERE id IN (${id});`;
+    const result = await dbQuery(queryString);
+    return result;
+  },
+  getContactsByLinkedId: async ({ linkedId }) => {
+    let queryString = `SELECT * FROM CONTACT WHERE linkedId IN (${linkedId});`;
+    const result = await dbQuery(queryString);
+    return result;
+  },
   createContacts: async ({ model }) => {
     const queryString = `INSERT INTO CONTACT (${contactTableKeyNames.join(',')}) VALUES (${
-      model.phoneNumber || 'null'
-    }, ${model.email || 'null'}, ${model.linkedId}, "${model.linkPrecedence}", "${
-      model.createdAt
-    }", "${model.updatedAt}", ${model.deletedAt});`;
+      model.phoneNumber || 'NULL'
+    }, ${model.email ? `"${model.email}"` : 'NULL'}, ${model.linkedId}, "${
+      model.linkPrecedence
+    }", "${model.createdAt}", "${model.updatedAt}", ${model.deletedAt});`;
+    const result = await dbQuery(queryString);
+    return result.insertId;
+  },
+  updateContacts: async ({ ids, model }) => {
+    const queryString = `UPDATE CONTACT SET ${Object.keys(model).map(
+      (key) => `${key} = ${typeof model[key] === 'string' ? `"${model[key]}"` : `${model[key]}`}`
+    )} WHERE id IN (${ids});`;
     await dbQuery(queryString);
   }
 };
