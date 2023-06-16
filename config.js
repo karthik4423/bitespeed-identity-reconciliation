8c5b97b4a86cb02373b7ed1b072b874973ee6334
Karthik Raj <karthikraj4423@gmail.com>
2023-06-17 01:49:32 +0530

    updated db creds

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const config = {
   host: 'db-mysql-blr1-92842-do-user-5069941-0.b.db.ondigitalocean.com',
   port: 25060,
   database: 'bitespeed',
-  user: 'doadmin',
-  password: 'AVNS_GiYZPjsB9jNH-XJi-JJ'
+  user: 'karthik',
+  password: 'AVNS_ZEuN9afpF17MBO6Gf5M'
 };
 
 module.exports = config;
